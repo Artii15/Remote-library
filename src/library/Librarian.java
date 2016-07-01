bfd52eb10e461cdf365788f60f849df9865d18e6
Artii15 <artur.kowalski15@gmail.com>
2016-07-01 11:57:43 +0200

    Project structure

---
+++ src/library/Librarian.java
@@ -0,0 +1,7 @@
+package library;
+
+public class Librarian {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/library/Server.java
@@ -0,0 +1,7 @@
+package library;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
